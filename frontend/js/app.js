3db96170da1cfb7a4272d6b35ee411092cdf19c2
macleginn <dsnikolaev@gmail.com>
2016-07-01 16:43:41 +0300

    Removed js warning

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,9 @@
 var app = angular.module("queryApp", []);
 app.filter('formatList', function() {
     return function(arr) {
+    	if (arr === undefined) {
+    		arr = [];
+    	}
         return arr.join(", ");
     };
 });
